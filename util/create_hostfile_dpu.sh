4635bd925ec350b7074c23f5ce0fe2932ca14d95
ISC SCC03  () <isc_scc03@login02.hpcadvisorycouncil.com>
2022-05-10 08:29:17 -0700

    Erases hostfile and dpufile, to check if there was an error

--- util/create_hostfile_dpu.sh
+++ util/create_hostfile_dpu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PPN="${1:-32}"
 
+rm hostfile dpufile
 
 srun -l hostname -s | awk '{print $2 ":"'"$PPN}" | grep -v bf | sort > hostfile
 srun -l hostname -s | awk '{print $2}' | grep bf | sort |uniq > dpufile

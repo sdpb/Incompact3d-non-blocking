aab322430e02b136d17f7010c3afabfd4dcbb045
ISC SCC03  () <isc_scc03@login01.hpcadvisorycouncil.com>
2022-05-12 00:45:06 -0700

    Added launching scripts

--- launcher.sh
+++ launcher.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+set -e
 
-for PPN in  32
+examples_dir=`realpath examples`
+scDPU=`realpath sbatchDPU.sh`
+scNODPU=`realpath sbatchNODPU.sh`
+
+for PPN in 32 16 8 4
 do
-    cd examples
-    dir=ISC_PPN${PPN}_$(date "+%Y-%m-%d_%H-%M-%S")
-    cp -r ISC-base $dir
+    dir=/global/scratch/users/isc_scc03/ISC_DPU_NODES_09a16_PPN${PPN}_$(date "+%Y-%m-%d_%H-%M-%S")
+    cp -r $examples_dir/ISC-base $dir
     cd $dir
+    sbatch -p thor -N 16 --nodelist=thor[009-016],thor-bf[09-16] -J portedDPU_ISC_input_PPN${PPN} $scDPU ${PPN}
 
-    sbatch -p thor -N 16 --constraint="[thor-bf*8&thor*8]" -J portedDPU_ISC_input_PPN${PPN} ../../sbatchDPU.sh ${PPN}
+    dir=/global/scratch/users/isc_scc03/ISC_NODPU_PPN${PPN}_$(date "+%Y-%m-%d_%H-%M-%S")
+    cp -r $examples_dir/ISC-base $dir
+    cd $dir
+    sbatch -p thor -N 8 --constraint="thor*8" -J portedNODPU_ISC_input_PPN${PPN} $scNODPU ${PPN}
+    ../../sbatchDPU.sh ${PPN}
+    cd ../..
 done
--- sbatchNODPU.sh
+++ sbatchNODPU.sh
@@ -13,12 +13,11 @@ module load gcc/8.3.1
 module load mvapich2-dpu/2022.02
 
 srun -l hostname -s | awk '{print $2 ":"'"$PPN}" | grep -v bf | sort > hostfile
-srun -l hostname -s | awk '{print $2}' | grep bf | sort |uniq > dpufile
 
 NPROC=$((PPN * 8))
 EXE=/global/home/users/isc_scc03/xcompact-adios2-mvapich/Incompact3d-non-blocking/xcompact3d
 # DPU offload
 cp input.i3d.$PPN input.i3d
-mpirun_rsh -np $NPROC  -hostfile hostfile -dpufile dpufile $EXE
+mpirun_rsh -np $NPROC  -hostfile hostfile MV2_USE_DPU=0 $EXE
 set +e
 rm -rf data/ checkpoint restart-forces statistics/ probes/ out/ restart*
